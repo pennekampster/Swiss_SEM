357ef02a5ae840bed34327d2a335a0d48448c0e6
Frank Pennekamp <pennekampster@gmail.com>
2021-10-26 17:53:46 +0200

    Mostly working on live coding exercise

--- 2b_Exercises/Exercises.R
+++ 2b_Exercises/Exercises.R
@@ -9,7 +9,7 @@ set.seed(2397348)
 N <- 50
 dat <- data.frame(x1 = runif(N))
 dat$x2 = 0.9 * dat$x1 + runif(N)
-dat$x3 = 0.5 * dat$x2 + runif(N)
+dat$x3 = -0.5 * dat$x2 + runif(N)
 dat$y = 1.7 * dat$x1 + 0.8 * dat$x2 + runif(N) # + 0.9 * (dat$x1 * dat$x2) + runif(N)
 #dat$x1x2 = dat$x1 * dat$x2
 
@@ -20,27 +20,62 @@ ggplot(data=dat, aes(x=x3,y=y)) + geom_point() + stat_smooth(method="lm")
 summary(lm(y~x1+x2+x3, data=dat))
 visreg(lm(y~x1+x2+x3, data=dat), partial =T, gg=TRUE)
 
+# fit multiple regressions
 model <- ' 
 y ~ x1 + x2 + x3
 '
 
 fit <- sem(model, data=dat)
-summary(fit, fit.measures = TRUE, standardized=T)
-modindices(fit)
+summary(fit)
 
-model1 <- ' 
-y ~ x1 + x2 
-x2 ~ x1
+
+# Fit SEMs
+model2 <- ' 
+y ~ x1 + x2 + x3
+x2 ~ x1 
+'
+fit2 <- sem(model2, data=dat)
+summary(fit2)
+
+# which important paths have been omitted?
+modindices(fit2)
+subset(modindices(fit2), modindices(fit2)$mi > 3.84)
+
+# let's include additional path
+model3 <- ' 
+y ~ x1 + x2 + x3
+x2 ~ x1 
 x3 ~ x2
 '
 
-fit1 <- sem(model1, data=dat)
-summary(fit1, fit.measures = TRUE, standardized=T)
-modindices(fit1)
-
-aictab(list(fit, fit1))
+fit3 <- sem(model3, data=dat)
+summary(fit3, fit.measures = T)
 
 
+
+
+# model pruning?
+model_true <- ' 
+y ~ x1 + x2 
+x2 ~ x1
+x3 ~ x2
+
+# setting covariance to zero
+y ~~ 0*x3 
+'
+
+fit1 <- sem(model_true, data=dat)
+summary(fit1, fit.measures = T)
+
+# any more paths to include?
+subset(modindices(fit1), mi > 3.84)
+
+# compare nested fits
+anova(fit1, fit3)
+aictab(list(fit1, fit3))
+
+
+# Parameter labelling and derived quantities
 model1a <- ' 
 y ~ b1 * x1 +  b2 * x2 
 x2 ~ b3 * x1
@@ -54,7 +89,44 @@ tot.x1.y := b3 * b2 + b1
 
 fit1a <- sem(model1a, data=dat)
 summary(fit1a, fit.measures = F, standardized=T)
-modindices(fit2c)
+
+
+# Fixing parameters
+
+model1b <- ' 
+y ~ b1 * x1 +  b2 * x2 
+x2 ~ b3 * x1
+x3 ~ b4 * x2
+
+b3 == 0
+
+dir.x1.y := b1
+indir.x1.y := b3 * b2
+tot.x1.y := b3 * b2 + b1 
+'
+
+fit1b <- sem(model1b, data=dat)
+summary(fit1b, fit.measures = F, standardized=T)
+
+# diagnostics
+resid(fit1, type="cor")
+
+
+# interpretation
+
+# table of parameter estimates (confidence intervals)
+parameterEstimates(fit1)
+subset(parameterEstimates(fit1, standardized=T), op == "~")
+
+# get standardized estimates, SE and CIs
+standardizedSolution(fit1, type = "std.all")
+
+# standardized parameters and R square values (std.lv vs std.all)
+summary(fit1, standardized=T, rsq=T)
+
+
+
+
 
 
 
