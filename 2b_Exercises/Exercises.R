e2618ee979f54714f18bb68a95a25ed01f7351c5
Frank Pennekamp <pennekampster@gmail.com>
2021-11-02 18:05:15 +0100

    Re-arranging the interaction slide deck at end of exercises (following course structure)

--- 2b_Exercises/Exercises.R
+++ 2b_Exercises/Exercises.R
@@ -11,7 +11,6 @@ dat <- data.frame(x1 = runif(N))
 dat$x2 = 0.9 * dat$x1 + runif(N)
 dat$x3 = -0.5 * dat$x2 + runif(N)
 dat$y = 1.7 * dat$x1 + 0.8 * dat$x2 + runif(N) # + 0.9 * (dat$x1 * dat$x2) + runif(N)
-#dat$x1x2 = dat$x1 * dat$x2
 
 ggplot(data=dat, aes(x=x1,y=y)) + geom_point() + stat_smooth(method="lm")
 ggplot(data=dat, aes(x=x2,y=y)) + geom_point() + stat_smooth(method="lm")
@@ -142,14 +141,246 @@ standardizedSolution(fit1, type = "std.all")
 summary(fit1, standardized=T, rsq=T)
 
 
+# Exercise: derived quantities
+#
+# Add variables that calculate the total, direct and indirect effect of each variable
 
+
+
+# Mediation exercise after fitting first SEM:
+
+# Let's test whether the effect of disturbance is mediated via its
+# effect on richness and eveness, rather than directly on biomass
+# add paths from disk to rich and even, remove the path to mass.above
+# compare model fit to simple model
+# what do you conclude?
+
+modindices(fit.simple.up, minimum.value = 0.01)
+
+simple.exc <-
+"mass.above ~ nadd + rich + even + precip.mm 
+rich ~ nadd + precip.mm + disk
+even ~ nadd + precip.mm +  disk
+rich ~~ even"
+
+fit.simple.exc <- sem(simple.exc, data = seabloom, estimator = "MLM")
+summary(fit.simple.exc, fit.measures = TRUE, rsq = TRUE)
+modindices(fit.simple.exc, minimum.value = 0.01)
+
+AIC(fit.simple.up, fit.simple.exc)
+
+
+# Exercise to construct latent variables
+library(faux)
+
+# Accounting for measurement error with 2 indicator variables
+
+set.seed(6553454)
+dat <- rnorm_multi(n = 50, 
+                   mu = c(10, 10),
+                   sd = c(1, 1),
+                   r = c(0.9), 
+                   varnames = c("M1", "M2"),
+                   empirical = F)
+
+dat$Xi = 0.3 * dat$M1 + 0.3 * dat$M2
+dat$y <- dat$Xi + runif(50)
+
+pairs(dat)
+cor.test(dat$M1, dat$M2)
+
+
+cfa <- "y ~ M1"
+fit <- cfa(cfa, data=dat)
+summary(fit, standardized=T, rsq=T)
+
+
+latent <- '
+xi =~ lambda*M1 + lambda*M2 # exogenous latent
+
+eta =~ y # endogenous latent
+
+eta ~ xi # path model
+'
+fit <- cfa(latent, data=dat)
+summary(fit, standardized=T, rsq=T)
+
+
+# More than two indicator variables
+set.seed(72643276)
+dat <- rnorm_multi(n = 100, 
+                   mu = c(20, 20, 20),
+                   sd = c(5, 5, 5),
+                   r = c(0.4, 0.5, 0.3), 
+                   varnames = c("length", "width", "mass"),
+                   empirical = F)
+
+pairs(dat)
+
+cfa <- "body_size =~ mass + width + length"
+fit <- cfa(cfa, data=dat)
+summary(fit, standardized=T, rsq=T)
+
+
+
+
+
+
+# Exercise 2 indicator latent variables
+cor.test(seabloom$precip.gs, seabloom$precip.mm)
+plot(seabloom$precip.gs, seabloom$precip.mm)
+
+seabloom$precip.mm_std <- (mean(seabloom$precip.mm)-seabloom$precip.mm) / sd(seabloom$precip.mm)
+seabloom$precip.gs_std <- (mean(seabloom$precip.gs)-seabloom$precip.gs) / sd(seabloom$precip.gs)
+
+# CFA
+lv <-"
+# Latent variable definition
+climate =~ lambda*precip.mm_std + lambda*precip.gs_std
+"
+
+fit.lv <- cfa(lv, data = seabloom, estimator = "MLM")
+summary(fit.lv, standardized=T)
+modindices(fit.lv, minimum.value = 3.84)
+
+# Full model
+lv <-"
+# Latent variable definition
+climate =~ lambda*precip.mm_std + lambda*precip.gs_std
+
+mass.above ~ nadd + disk + climate
+rich ~ nadd + climate + disk
+even ~ nadd + climate + disk"
+
+fit.lv <- sem(lv, data = seabloom, estimator = "MLM")
+summary(fit.lv, standardized=T)
+modindices(fit.lv, minimum.value = 3.84)
+
+
+
+
+
+# Exercise to construct composite variables
+comp2 <- "
+landuse2 <~  0.06544565 * nadd + 0.4466863 * disk
+
+rich ~ landuse2 + precip.mm
+even ~ landuse2 + precip.mm
+
+mass.above ~ landuse2 + rich + even + precip.mm
+
+rich ~~ even"
+
+fit.comp2 <- sem(comp2, data = seabloom)
+summary(fit.comp2, standardize = T)
+
+
+
+comp2 <- "
+landuse2 <~   nadd + 1 * disk
+
+rich ~ landuse2 + precip.mm
+even ~ landuse2 + precip.mm
+
+mass.above ~ landuse2 + rich + even + precip.mm
+
+rich ~~ even"
+
+fit.comp2 <- sem(comp2, data = seabloom, )
+summary(fit.comp2, standardize=T)
+
+
+
+# Composite variables are convenient to model nonlinear relationships
+
+ggplot(data=seabloom, aes(precip.gs, mass.above)) + geom_point() + 
+  stat_smooth(method="lm", formula = "y ~ poly(x, 2)", se=F) + 
+  stat_smooth(method="lm", formula = "y ~ x", colour="red")
+
+# Exercise to construct composite variables
+
+seabloom$precip.gs.std <- (mean(seabloom$precip.gs)-seabloom$precip.gs) / sd(seabloom$precip.gs)
+seabloom$precip.gs.sq.std <- seabloom$precip.gs.std ^ 2
+
+# Fit nonlinear composite in one step
+
+comp2 <- "
+
+precip_tot <~  precip.gs.sq.std + precip.gs.std 
+
+rich ~ nadd + precip_tot
+even ~ nadd + precip_tot
+
+mass.above ~ rich + even + precip_tot + nadd
+
+rich ~~ even"
+
+fit.comp2 <- sem(comp2, data = seabloom)
+summary(fit.comp2, standardize = T)
+
+
+# Fit nonlinear composite in two steps
+
+comp2 <- "mass.above ~  precip.gs.sq.std + precip.gs.std"
+
+fit.comp2 <- sem(comp2, data = seabloom)
+summary(fit.comp2, standardize = T)
+
+
+comp2 <- "
+
+precip_tot <~  -0.216 * precip.gs.sq.std + -0.277 * precip.gs.std 
+
+rich ~ nadd + precip_tot
+even ~ nadd + precip_tot
+
+mass.above ~ rich + even + precip_tot + nadd
+
+rich ~~ even"
+
+fit.comp2 <- sem(comp2, data = seabloom)
+summary(fit.comp2, standardize = T, fit.measures=T)
+
+
+
+# compare with model that includes the squared term
+comp2 <- "
+
+rich ~ nadd + precip.gs.std + precip.gs.sq.std
+even ~ nadd + precip.gs.std
+
+mass.above ~ rich + even + precip.gs.std + precip.gs.sq.std + nadd
+
+rich ~~ even"
+
+fit.comp2 <- sem(comp2, data = seabloom)
+summary(fit.comp2, standardize = T, fit.measures=T)
+
+
+
+# compare with model that includes the squared term
+comp2 <- "
+
+rich ~ nadd + precip.gs.std
+even ~ nadd + precip.gs.std
+
+mass.above ~ rich + even + precip.gs.std + nadd
+
+rich ~~ even"
+
+fit.comp2 <- sem(comp2, data = seabloom)
+summary(fit.comp2, standardize = T, fit.measures=T)
 
 
 
 
 
 # interaction
-
+set.seed(2397348)
+N <- 50
+dat <- data.frame(x1 = runif(N))
+dat$x2 = 0.9 * dat$x1 + runif(N)
+dat$x3 = -0.5 * dat$x2 + runif(N)
 dat$y = 1.7 * dat$x1 + 0.8 * dat$x2 + 0.9 * (dat$x1 * dat$x2) + runif(N)
 dat$x1x2 = dat$x1 * dat$x2
 
@@ -198,185 +429,6 @@ modindices(fit2c)
 
 
 aictab(list(fit2a, fit2b, fit2c))
-
-
-
-
-
-
-
-
-
-
-# Exercise: derived quantities
-#
-# Add variables that calculate the total, direct and indirect effect of each variable
-
-
-
-
-
-
-# First exercise after fitting first SEM:
-
-# Let's test whether the effect of disturbance is mediated via its
-# effect on richness and eveness, rather than directly on biomass
-# add paths from disk to rich and even, remove the path to mass.above
-# compare model fit to simple model
-# what do you conclude?
-
-modindices(fit.simple.up, minimum.value = 0.01)
-
-simple.exc <-
-"mass.above ~ nadd + rich + even + precip.mm 
-rich ~ nadd + precip.mm + disk
-even ~ nadd + precip.mm +  disk
-rich ~~ even"
-
-fit.simple.exc <- sem(simple.exc, data = seabloom, estimator = "MLM")
-summary(fit.simple.exc, fit.measures = TRUE, rsq = TRUE)
-modindices(fit.simple.exc, minimum.value = 0.01)
-
-AIC(fit.simple.up, fit.simple.exc)
-
-
-# Exercise to construct latent variables
-library(faux)
-
-# Single indicator variable
-
-set.seed(6553454)
-dat <- rnorm_multi(n = 50, 
-                   mu = c(10, 10, 10),
-                   sd = c(1, 1, 1),
-                   r = c(0.9), 
-                   varnames = c("M1", "M2", "y"),
-                   empirical = F)
-
-pairs(dat)
-
-
-N <- 50
-dat <- data.frame(M1 = runif(N))
-dat$M2 = dat$M1 + runif(N)
-dat$Xi = 0.9 * dat$M1 + 0.9 * dat$M2
-dat$y <- dat$Xi + runif(N)
-
-
-cfa <- "y ~ M1"
-fit <- cfa(cfa, data=dat)
-summary(fit, standardized=T, rsq=T)
-
-
-latent <- '
-xi =~ lambda*M1 + lambda*M2 # exogenous latent
-
-eta =~ y # endogenous latent
-
-eta ~ xi # path model
-
-'
-fit <- cfa(latent, data=dat)
-summary(fit, standardized=T, rsq=T)
-
-
-
-
-# Multiple indicator variables
-set.seed(72643276)
-dat <- rnorm_multi(n = 100, 
-                   mu = c(10, 20, 20),
-                   sd = c(4, 5, 5),
-                   r = c(0.4, 0.1, 0.3), 
-                   varnames = c("length", "width", "mass"),
-                   empirical = F)
-
-pairs(dat)
-
-cfa <- "body_size =~ length + width + mass"
-fit <- cfa(cfa, data=dat)
-summary(fit, standardized=T, rsq=T)
-
-
-# Two factor latent variable
-dat$length_std <- (mean(dat$length)-dat$length) / sd(dat$length)
-dat$width_std <- (mean(dat$width)-dat$width) / sd(dat$width)
-
-cfa <- "body_size =~ lambda*length_std + lambda*width_std"
-fit <- cfa(cfa, data=dat)
-summary(fit, standardized=T, rsq=T)
-
-
-# Exercise 2 indicator latent variables
-cor.test(seabloom$precip.gs, seabloom$precip.mm)
-plot(seabloom$precip.gs, seabloom$precip.mm)
-
-seabloom$precip.mm_std <- (mean(seabloom$precip.mm)-seabloom$precip.mm) / sd(seabloom$precip.mm)
-seabloom$precip.gs_std <- (mean(seabloom$precip.gs)-seabloom$precip.gs) / sd(seabloom$precip.gs)
-
-
-
-lv <-"
-# Latent variable definition
-climate =~ lambda*precip.mm_std + lambda*precip.gs_std
-"
-
-fit.lv <- cfa(lv, data = seabloom, estimator = "MLM")
-summary(fit.lv, standardized=T)
-modindices(fit.lv, minimum.value = 3.84)
-
-lv <-"
-# Latent variable definition
-climate =~ lambda*precip.mm_std + lambda*precip.gs_std
-
-mass.above ~ nadd + disk + climate
-rich ~ nadd + climate + disk
-even ~ nadd + climate + disk"
-
-fit.lv <- sem(lv, data = seabloom, estimator = "MLM")
-summary(fit.lv, standardized=T)
-modindices(fit.lv, minimum.value = 3.84)
-
-
-
-
-
-# Exercise to construct composite variables
-
-# Composite variables are convenient to model nonlinear relationships
-
-ggplot(data=seabloom, aes(precip.gs, mass.above)) + geom_point() + 
-  stat_smooth(method="lm", formula = "y ~ poly(x, 2)", se=F) + 
-  stat_smooth(method="lm", formula = "y ~ x", colour="red")
-
-
-comp2 <- "
-landuse2 <~  0.06544565 * nadd + 0.4466863 * disk
-
-rich ~ landuse2 + precip.mm
-even ~ landuse2 + precip.mm
-
-mass.above ~ landuse2 + rich + even + precip.mm
-
-rich ~~ even"
-
-fit.comp2 <- sem(comp2, data = seabloom)
-summary(fit.comp2, standardize = T)
-
-
-
-comp2 <- "
-landuse2 <~   nadd + 1 * disk
-
-rich ~ landuse2 + precip.mm
-even ~ landuse2 + precip.mm
-
-mass.above ~ landuse2 + rich + even + precip.mm
-
-rich ~~ even"
-
-fit.comp2 <- sem(comp2, data = seabloom, )
-summary(fit.comp2, standardize=T)
 
 
 
