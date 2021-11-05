a8a16c74aa38b188644fabc2ea90a9cfba5edf87
Frank Pennekamp <pennekampster@gmail.com>
2021-11-05 11:30:54 +0100

    Fixed the simulations to show use of composites to model interactions

--- 2b_Exercises/Live_demoSEM2.R
+++ 2b_Exercises/Live_demoSEM2.R
@@ -1,5 +1,7 @@
 # Live demo latent and composite variables
 library(faux)
+library(lavaan)
+library(AICcmodavg)
 
 # Accounting for measurement error with 2 indicator variables
 
@@ -34,6 +36,10 @@ fit <- cfa(latent, data=dat)
 summary(fit, standardized=T, rsq=T)
 
 
+
+
+
+
 # More than two indicator variables
 set.seed(72643276)
 dat <- rnorm_multi(n = 100, 
@@ -55,53 +61,46 @@ summary(fit, standardized=T, rsq=T)
 set.seed(2397348)
 N <- 50
 dat <- data.frame(x1 = runif(N))
-dat$x2 = 0.9 * dat$x1 + runif(N)
+dat$x2 = runif(N)
+dat$x1x2 = dat$x1 * dat$x2
 dat$x3 = -0.5 * dat$x2 + runif(N)
-dat$y = 1.7 * dat$x1 + 0.8 * dat$x2 + 0.9 * (dat$x1 * dat$x2) + runif(N)
+dat$y = 1.7 * dat$x1 + 0.8 * dat$x2 + -0.8 * dat$x1x2 + runif(N)
 dat$x1x2 = dat$x1 * dat$x2
 
-lm(y ~ x1 +  x2 + x1x2, data=dat)
+pairs(dat)
+
+summary(lm(y ~ x1 * x2 * x3, data=dat))
 
 model2a <- ' 
+comp.int <~ 1 * x1 + x2 + x1x2 
+x3 ~x2
 y ~ comp.int 
-x2 ~ x1
-x3 ~ x2
-
-comp.int <~ 1.5745 * x1 +  0.7477 * x2 + 0.9668 * x1x2
 '
 
 fit2a <- sem(model2a, data=dat)
 summary(fit2a, fit.measures = TRUE, standardized=T)
 modindices(fit2a)
 
+
+summary(lm(y ~ x1 * x2, data=dat))
+
 model2b <- ' 
-y ~ comp.int 
-x2 ~ x1
+
 x3 ~ x2
 
-comp.int <~ 1 * x1 +  x2 + x1x2
-
+y ~ 1.7579 * x1 +   0.9493 * x2 + -1.0733 * x1x2
 '
 
 fit2b <- sem(model2b, data=dat)
 summary(fit2b, fit.measures = TRUE, standardized=T)
-modindices(fit2b)
+
 
 model2c <- ' 
 y ~ b1 * x1 +  b2 * x2 + b3 * x1x2 
-x2 ~ b4 * x1
 x3 ~ b5 * x2
-
-
-ind.x1.y := b4 * b2
-ind.x1.x3 := b5 * b2
-
-tot.x1.y := b4 * b2 + b1 
 '
 
 fit2c <- sem(model2c, data=dat)
 summary(fit2c, fit.measures = TRUE, standardized=T)
 modindices(fit2c)
 
-
-aictab(list(fit2a, fit2b, fit2c))
