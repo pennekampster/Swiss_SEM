b70e87655e295d80c6e990d42346c9f8472cd4ee
Frank <pennekampster@gmail.com>
2024-01-18 08:59:03 +0100

    Small fixes to exercise and live demo of day 2

--- 2b_Exercises/Live_demo_SEM2.R
+++ 2b_Exercises/Live_demo_SEM2.R
@@ -5,7 +5,7 @@ library(faux)
 
 ## Latent variables
 
-# CFA with more than two indicator variables
+# CFA with more than three indicator variables
 set.seed(72643276)
 dat <- rnorm_multi(n = 100, 
                    mu = c(4, 20, 1),
@@ -21,11 +21,9 @@ fit <- cfa(cfa, data=dat)
 summary(fit, standardized=T, rsq=T)
 
 
-
-
 # Accounting for measurement error with 2 indicator variables
 set.seed(1)
-n = 1000
+n = 100
 x = rnorm(n)
 
 eta1 = rnorm(n) # measurement error1
@@ -43,9 +41,9 @@ y = beta0+beta1*x + u
 
 dat <- data.frame(y, eta1, eta2, noisy1, noisy2)
 
-# attenuation bias
+# regression dilution 7 attenuation bias
 lm <- '
-y ~ noisy2 # exogenous latent
+y ~ noisy1 # exogenous latent
 '
 fit <- sem(lm, data=dat)
 summary(fit, standardized=T, rsq=T)
@@ -80,18 +78,22 @@ dat$y = 1.7 * dat$x1 + 0.8 * dat$x2 + -0.8 * dat$x1x2 + rnorm(N)
 
 pairs(dat)
 
-summary(lm(y ~ x1 * x2, data=dat))
+summary(lm(y ~ x1 + x2 + x1:x2, data=dat))
 
 # fit composite in lavaan (automatically)
 
 model2a <- ' 
-comp.int <~ 1 * x1 + x2 + x1x2 
+comp.int <~  1* x1 + x2 + x1x2 
 y ~ comp.int 
 '
 
 fit2a <- sem(model2a, data=dat)
 summary(fit2a, standardized=T)
 modindices(fit2a)
+
+
+
+
 
 # fit composite in lavaan (manually)
 
@@ -102,12 +104,24 @@ coef(lm(y ~ x1 * x2, data=dat))
 
 model2b <- ' 
 comp.int <~ 1.72075 * x1 +   0.43481 * x2 + -0.67734  * x1x2
-
 y ~ comp.int
 '
 
 fit2b <- sem(model2b, data=dat)
 summary(fit2b, standardized=T)
+
+dat$comp.int <- 1.72075 * dat$x1 +   0.43481 * dat$x2 + -0.67734  * dat$x1x2
+  
+  
+model2b <- ' 
+y ~ comp.int
+'
+
+fit2b <- sem(model2b, data=dat)
+summary(fit2b, standardized=T)
+
+
+
 
 # compare output to show that factor loadings are the same when standardized
 
@@ -128,22 +142,33 @@ modindices(fit2c)
 N <- 50
 set.seed(2397348)
 dat <- data.frame(x1 = rnorm(N))
-dat$group = rep(c("1","2"), each = N/2)
+dat$group = rep(c("0","1"), each = N/2)
 dat$y <- ifelse(dat$group == "1", 0.2 * dat$x1, 0.9 * dat$x1) + rnorm(N, 0, .1)
-
+dat$group_num <- as.numeric(dat$group)
 # ANCOVA
 summary(lm(y~x1 * group, data=dat))
 
 model3a <- ' 
-y ~ x1
+y ~ x1 
 '
 
 fit3a <- sem(model3a, group = "group", data=dat)
 summary(fit3a)
 
+dat$int_term <- dat$x1 * dat$group_num
+
+
+model3a <- ' 
+y ~ x1 + group_num + int_term
+'
+
+fit3a <- sem(model3a, data=dat)
+summary(fit3a)
+
 
 model3b <- ' 
-y ~ c("b1", "b1") * x1
+y ~ c("b1", "b2") * x1
+b1 == 0
 '
 
 fit3b <- sem(model3b, group = "group", data=dat)
